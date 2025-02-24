d67734de4e96415a0b3258d21746ec9793ec0299
Wei Cheng <32865575+games130@users.noreply.github.com>
2025-02-24 15:29:54 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# media-stream-save-audio
+# Connect Basic Demo
+
+This is a basic server application that consumes audio from Twilio Media Streams and save the audio to file
+This is using a library from https://github.com/jremi/twilio-media-stream-save-audio-file, if you want to write your own logic you can refer to this / https://stackoverflow.com/questions/58439005/is-there-any-way-to-save-mulaw-audio-stream-from-twilio-in-a-file
+
+## App sever setup
+
+### Installation
+
+**Requires Node >= v12.1.0**
+
+Run `npm install`
+
+npm dependencies (contained in the `package.json`):
+* twilio
+* ws
+* twilio-media-stream-save-audio-file
+
+## Setup
+
+1. Run `npm install`
+2. Run the app by running `node ./server.js`
+3. By default, the app will be bound to localhost on port 3000
+4. To expose the app to the internet, you can use ngrok by running the command ngrok http 3000
+5. In the Twilio Console, use the ngrok URL as your Voice URL, appending /twiml for the phone number you would like to use. For example - https://123456789.ngrok.io/twiml
+6. You will also need to replace the URL in variable name `SERVER_URL` in the server.js file, for example - 123456789.ngrok.io
---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const fs = require("fs");
+const twilio = require('twilio');
+const path = require("path");
+const TwilioMediaStreamSaveAudioFile = require("twilio-media-stream-save-audio-file");
+var http = require("http");
+const { Server } = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new Server({ server });
+
+const mediaStreamSaver = new TwilioMediaStreamSaveAudioFile({
+  saveLocation: __dirname,
+  saveFilename: "my-twilio-media-stream-output",
+  onSaved: () => console.log("File was saved!"),
+});
+
+
+  // 📢 Twilio XML Response to Connect WebSocket
+  app.post('/twiml', (req, res) => {
+    console.log("📞 Incoming call from Twilio");
+
+    const twiml = new twilio.twiml.VoiceResponse();
+    twiml.say("Hello! Welcome to to this test call. Please say something");
+    
+    twiml.connect().stream({
+        url: `wss://f5ae5a6a182d.ngrok.app/`
+    });
+
+    console.log("🛠️ Twilio Stream URL:");
+    
+    res.type('text/xml').send(twiml.toString());
+  });
+
+  // 🎤 WebSocket Server for Streaming Audio
+  wss.on('connection', (ws) => {
+    console.log("🔗 WebSocket Connected! Waiting for audio input...");
+
+    ws.on('message', (message) => {
+      console.log(`🔊 Received ${message.length} bytes of audio from Twilio`);
+      const msg = JSON.parse(message);
+      switch (msg.event) {
+        case "connected":
+          console.log("A new call has connected");
+          break;
+        case "start":
+          console.log(`Starting media stream...`);
+          mediaStreamSaver.twilioStreamStart();
+          break;
+        case "media":
+          console.log("Receiving audio...");
+          mediaStreamSaver.twilioStreamMedia(msg.media.payload);
+          break;
+        case "stop":
+          console.log("Call has ended");
+          mediaStreamSaver.twilioStreamStop();
+          break;
+        default:
+          break;
+      }
+    });
+
+    ws.on('close', () => {
+      console.log("❌ WebSocket Disconnected!");
+    });
+  });
+
+// ✅ Start Server
+server.listen(3000, () => console.log("✅ Server running on port 3000"));
